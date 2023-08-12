45a9ea4e754962ffed9a782941d9f8a7951d9ada
Furkan Cingoz <info@furkancingoz.com>
2023-08-13 01:42:11 +0300

    Initial Commit

---
+++ SwiftUIFireBaseChat/SwiftUIFireBaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFireBaseChatApp.swift
+//  SwiftUIFireBaseChat
+//
+//  Created by Furkan Cingöz on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFireBaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
